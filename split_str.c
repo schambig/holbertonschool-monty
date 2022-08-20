1dcaef5371bbfd89d1b7d991666ade918845dc15
Salomon Chambi <salomonchambi@gmail.com>
2022-08-20 09:17:17 -0700

    Refactor: delete unnecessary delimiter

--- split_str.c
+++ split_str.c
@@ -11,10 +11,10 @@ char *split_str(char *str_to_split)
 	char *op_code = NULL;
 	char *argument = NULL;
 
-	op_code = strtok(str_to_split, " \n\t\r");
+	op_code = strtok(str_to_split, " \n\t");
 	if (strcmp(op_code, "push") == 0)
 	{
-		argument = strtok(NULL, " \n\t\r");
+		argument = strtok(NULL, " \n\t");
 		is_digit(argument);
 		var.node_data = atoi(argument);
 	}
